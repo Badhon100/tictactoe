7c9d33f3638ffca4ec9629bd5c182c2bc6455244
Badhon100 <badhonx102@gmail.com>
2020-07-04 14:33:52 +0600

    fix cross and circle

--- home.js
+++ home.js
@@ -1,6 +1,9 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+
 function tictactoe1() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
+  
   ctx.moveTo(20, 20);
   ctx.lineTo(300, 150);
   ctx.stroke();
@@ -11,10 +14,21 @@ function tictactoe1() {
 }
 
 function tictactoe2(){
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
+  
   ctx.beginPath();
   ctx.arc(150, 75, 60, 0, 2 * Math.PI);
   ctx.stroke();
 }
 
+function show(){
+
+
+    let i = 2;
+        if(i == 3){
+            tictactoe1();
+        }
+        else{
+            tictactoe2();
+        }
+    
+}
