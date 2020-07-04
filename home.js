643f1c65b9a4efa9e2600bca64fa287d91f4a93d
Badhon100 <badhonx102@gmail.com>
2020-07-04 14:18:11 +0600

    creating circle and cross

--- home.js
+++ home.js
@@ -1,5 +1,20 @@
+function tictactoe1() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  ctx.moveTo(20, 20);
+  ctx.lineTo(300, 150);
+  ctx.stroke();
 
-function name(nme){
-    document.write("hello " + nme);
+  ctx.moveTo(0, 150);
+  ctx.lineTo(280, 20);
+  ctx.stroke();
 }
-name("badhon");
+
+function tictactoe2(){
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(150, 75, 60, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
